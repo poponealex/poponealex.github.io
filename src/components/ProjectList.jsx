8916f855c1e0a6571d77eae9c3269874968bbcc3
Alexandre Perlmutter <alexandre@perlmutter.tech>
2024-01-22 13:48:19 +0100

    Improve project list

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,54 +1,104 @@
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { EllipsisHorizontalIcon } from '@heroicons/react/20/solid'
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid";
+import { default as Alert } from "./AlertWIP.jsx";
+
+import { useState } from "react";
 
 const projects = [
   {
-    name: 'rootmim',
-    imageUrl: 'https://www.rootmim.org/logo.png',
-    date: '2021 - present',
-    url: 'https://www.rootmim.org',
+    name: "{rootmim}",
+    short_description: "Ethical hacking and cybersecurity training platform",
+    imageUrl: "/logo-rootmim.png",
+    link: { short: "rootmim.org", url: "https://www.rootmim.org" },
+    kind: "Fullstack hacking platform",
+    stack: "Express.js, React, Docker, C, OpenVPN, SQL, Bash, Linux, Proxmox",
   },
   {
-    name: 'cert-manager-routes-controller',
-    imageUrl: 'https://raw.githubusercontent.com/its4u/cert-manager-routes-controller/main/img/openshift-cert-manager-logo.png',
-    date: '2023',
-    url: 'https://github.com/its4u/cert-manager-routes-controller',
+    name: "cert-manager-routes-controller",
+    kind: "RHOCP controller",
+    short_description:
+      "An Openshift controller to integrate cert-manager with OpenShift Routes",
+    imageUrl: "/logo-cert-manager-routes-controller.png",
+    link: {
+      short: "its4u/cert-manager-routes-controller",
+      url: "https://github.com/its4u/cert-manager-routes-controller",
+    },
+    stack: "Rust, RHOCP, Helm",
   },
   {
-    name: 'Suprenam',
-    imageUrl: 'https://raw.githubusercontent.com/poponealex/suprenam/master/img/logo_small.png',
-    date: '',
-    url: 'https://github.com/poponealex/suprenam',
+    name: "Suprenam",
+    kind: "App",
+    short_description:
+      "It's Suprenam, a batch renaming utility which relies on the tool you know best: your text editor",
+    imageUrl: "/logo-suprenam.png",
+    link: {
+      short: "poponealex/suprenam",
+      url: "https://github.com/poponealex/suprenam",
+    },
+    stack: "Python",
   },
   {
-    name: 'Collectif Poncelet',
-    imageUrl: 'https://tailwindui.com/img/logos/48x48/reform.svg',
-    date: '',
-    url: '',
+    name: "Collectif Poncelet",
+    kind: "Website",
+    short_description:
+      "A neighborhood civil rights association that aims to protect the residents of downtown Metz",
+    imageUrl: "/logo-cp.png",
+    link: {
+      short: "collectifponcelet.fr",
+      url: "https://www.collectifponcelet.fr",
+    },
+    stack: "Rust, React",
   },
-]
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Example() {
+  const [show, setShow] = useState(false);
   return (
-    <ul role="list" className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
+    <>
+    <div className="mb-8">
+    {show && (<Alert
+      message="Project's page under development!"
+      setShow={setShow}
+    />)}
+    </div>
+    <ul
+      role="list"
+      className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8"
+    >
+      
       {projects.map((project) => (
-        <li key={project.id} className="overflow-hidden rounded-xl border border-gray-200">
+        <li
+          key={project.id}
+          className="overflow-hidden rounded-xl border border-gray-200"
+        >
           <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6">
             <img
               src={project.imageUrl}
               alt={project.name}
               className="h-12 w-12 flex-none rounded-lg bg-white object-cover ring-1 ring-gray-900/10"
             />
-            <div className="text-sm font-medium leading-6 text-gray-900">{project.name}</div>
+            <div>
+              <p className="text-sm font-medium leading-6 text-gray-900">
+                <a onClick={() => setShow(true)}>
+                  {project.name}
+                </a>
+              </p>
+              <p className="text-xs font-medium leading-6 text-gray-600">
+                {project.short_description}
+              </p>
+            </div>
             <Menu as="div" className="relative ml-auto">
               <Menu.Button className="-m-2.5 block p-2.5 text-gray-400 hover:text-gray-500">
                 <span className="sr-only">Open options</span>
-                <EllipsisHorizontalIcon className="h-5 w-5" aria-hidden="true" />
+                <EllipsisHorizontalIcon
+                  className="h-5 w-5"
+                  aria-hidden="true"
+                />
               </Menu.Button>
               <Transition
                 as={Fragment}
@@ -63,26 +113,14 @@ export default function Example() {
                   <Menu.Item>
                     {({ active }) => (
                       <a
-                        href="#"
+                        onClick={() => setShow(true)}
+                        // href="#"
                         className={classNames(
-                          active ? 'bg-gray-50' : '',
-                          'block px-3 py-1 text-sm leading-6 text-gray-900'
+                          active ? "bg-gray-50" : "",
+                          "block px-3 py-1 text-sm leading-6 text-gray-900"
                         )}
                       >
                         View<span className="sr-only">, {project.name}</span>
-                      </a>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={classNames(
-                          active ? 'bg-gray-50' : '',
-                          'block px-3 py-1 text-sm leading-6 text-gray-900'
-                        )}
-                      >
-                        Edit<span className="sr-only">, {project.name}</span>
                       </a>
                     )}
                   </Menu.Item>
@@ -92,20 +130,31 @@ export default function Example() {
           </div>
           <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
             <div className="flex justify-between gap-x-4 py-3">
-              <dt className="text-gray-500">Date</dt>
-              <dd className="text-gray-700">
-                <time>{project.date}</time>
+              <dt className="text-gray-500">Kind</dt>
+              <dd className="flex items-start gap-x-2">
+                <div className="font-medium text-gray-900">{project.kind}</div>
               </dd>
             </div>
             <div className="flex justify-between gap-x-4 py-3">
-              <dt className="text-gray-500">URL</dt>
+              <dt className="text-gray-500">Stack</dt>
               <dd className="flex items-start gap-x-2">
-                <div className="font-medium text-gray-900">{project.url}</div>
+                <div className="font-medium text-gray-900">{project.stack}</div>
+              </dd>
+            </div>
+            <div className="flex justify-between gap-x-4 py-3">
+              <dt className="text-gray-500">Link</dt>
+              <dd className="flex items-start gap-x-2">
+                <div className="font-medium text-gray-900">
+                  <a href={project.link.url} target="_blank">
+                    {project.link.short}
+                  </a>
+                </div>
               </dd>
             </div>
           </dl>
         </li>
       ))}
     </ul>
-  )
+    </>
+  );
 }
